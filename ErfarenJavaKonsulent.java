f62216c873d61e46ce865a9253c498300bc46487
Anders Emil Salvesen <andersems@gmail.com>
2016-06-09 00:24:09 +0200

    Inititiell commit

---
+++ ErfarenJavaKonsulent.java
@@ -0,0 +1,7 @@
+"public class ErfarenJavaKonsulent {
+List<String> arbeidsoppgaver = Arrays.asList(
+""Enterprise Javautvikling"", 
+""Analyse og design"", 
+""Smidig utvikling"", 
+""TeknologirÂdgivning"");
+} Søk hos oss i dag!"
